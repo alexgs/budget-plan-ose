18668848b306549aa49d1b7f34aafb48046c9c6c
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-12 09:15:42 -0400

    Get GitHub login working

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,14 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import * as env from 'env-var';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
+import prisma from '../../../server-lib/prismadb';
+
+// TODO GitHub provider doesn't pull in my email address; maybe needs a scope or something? :shrug:
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: env.get('GITHUB_ID').required().asString(),
--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,18 +1,44 @@
 import styled from '@emotion/styled';
+import { getProviders, signIn, signOut, useSession } from 'next-auth/react';
 import { contentWidth, space } from '../components/tokens';
 
 const HomePageContainer = styled.div({
   margin: `${space.medium} auto`,
   maxWidth: contentWidth.small,
   textAlign: 'center',
-})
+});
 
-function HomePage() {
+interface Props {
+  providers: ReturnType<typeof getProviders>;
+}
+
+function HomePage(props: Props) {
+  const { providers } = props;
+  const { data: session } = useSession();
+  const content = session ? (
+      <>
+        Signed in as {session.user?.name} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    ) : Object.values(providers).map((provider) => (
+    <div key={provider.name}>
+      <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
+    </div>
+  ));
+
   return (
     <HomePageContainer>
       <h1>Budget Plan</h1>
+      {content}
     </HomePageContainer>
   );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
 }
 
 export default HomePage;
