f72cc55a1b46144801f418e522a55e89b5a0cb09
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-11 20:24:31 -0400

    Add Emotion

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '@fontsource/inter/variable.css';
+import 'normalize.css/normalize.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
