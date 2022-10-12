72e3aff414997eba6ec6e5e91349f579d684b091
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-11 20:36:46 -0400

    Add global styles

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { globalStyles } from '../components';
 import '@fontsource/inter/variable.css';
 import 'normalize.css/normalize.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
 }
