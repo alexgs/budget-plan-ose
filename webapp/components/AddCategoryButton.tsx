d46fbfa76ae703a84e08ea503b9b6e5e6c88a128
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-14 05:52:02 -0400

    Add parent category dropdown menu

--- webapp/components/AddCategoryButton.tsx
+++ webapp/components/AddCategoryButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, Space } from '@mantine/core';
+import { Button, Modal, NativeSelect, Space } from '@mantine/core';
 import { useSession } from 'next-auth/react';
 import { FC, useState } from 'react';
 import { TextInputWithFloatingLabel } from './Inputs/TextInputWithFloatingLabel';
@@ -6,6 +6,12 @@ import { TextInputWithFloatingLabel } from './Inputs/TextInputWithFloatingLabel'
 export const AddCategoryButton: FC = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [categoryName, setCategoryName] = useState('');
+
+  const parentCategories = [
+    { label: 'None', value: '0' },
+    { label: 'House', value: 'abc123' },
+    { label: 'Groceries', value: 'abc124' },
+  ];
 
   function handleClick(): void {
     setIsVisible(true);
@@ -25,6 +31,12 @@ export const AddCategoryButton: FC = () => {
           placeholder="Rainy day fund"
           onChange={setCategoryName}
           value={categoryName}
+        />
+        <Space h="lg" />
+        <NativeSelect
+          data={parentCategories}
+          placeholder="Pick one"
+          label="Nest under"
         />
       </Modal>
       <div>
