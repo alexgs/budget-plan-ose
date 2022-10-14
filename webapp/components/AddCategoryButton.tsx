d83fd0b6959ef9ed32c6f0145bd87b43f134ac3b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-14 05:42:41 -0400

    Add "Name" input field to "Add category" modal

--- webapp/components/AddCategoryButton.tsx
+++ webapp/components/AddCategoryButton.tsx
@@ -1,8 +1,11 @@
-import { Button, Modal } from '@mantine/core';
+import { Button, Modal, Space } from '@mantine/core';
+import { useSession } from 'next-auth/react';
 import { FC, useState } from 'react';
+import { TextInputWithFloatingLabel } from './Inputs/TextInputWithFloatingLabel';
 
 export const AddCategoryButton: FC = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [categoryName, setCategoryName] = useState('');
 
   function handleClick(): void {
     setIsVisible(true);
@@ -10,8 +13,19 @@ export const AddCategoryButton: FC = () => {
 
   return (
     <>
-      <Modal onClose={() => setIsVisible(false)} opened={isVisible} overlayBlur={3} title="Introduce yourself!">
-        Hello modal!
+      <Modal
+        onClose={() => setIsVisible(false)}
+        opened={isVisible}
+        overlayBlur={3}
+        title="Add new category"
+      >
+        <Space h="xs" />
+        <TextInputWithFloatingLabel
+          label="Name"
+          placeholder="Rainy day fund"
+          onChange={setCategoryName}
+          value={categoryName}
+        />
       </Modal>
       <div>
         <Button variant="outline" onClick={handleClick}>
---
+++ webapp/components/Inputs/TextInputWithFloatingLabel.tsx
@@ -0,0 +1,67 @@
+import { FC, useState } from 'react';
+import { TextInput, createStyles } from '@mantine/core';
+
+// Adapted from https://ui.mantine.dev/component/floating-label-input
+
+const useStyles = createStyles(
+  (theme, { floating }: { floating: boolean }) => ({
+    root: {
+      position: 'relative',
+    },
+
+    label: {
+      position: 'absolute',
+      zIndex: 2,
+      top: 7,
+      left: theme.spacing.sm,
+      pointerEvents: 'none',
+      color: floating ? theme.white : theme.colors.dark[3],
+      transition:
+        'transform 150ms ease, color 150ms ease, font-size 150ms ease',
+      transform: floating ? `translate(-${theme.spacing.sm}px, -28px)` : 'none',
+      fontSize: floating ? theme.fontSizes.xs : theme.fontSizes.sm,
+      fontWeight: floating ? 500 : 400,
+    },
+
+    required: {
+      transition: 'opacity 150ms ease',
+      opacity: floating ? 1 : 0,
+    },
+
+    input: {
+      '&::placeholder': {
+        transition: 'color 150ms ease',
+        color: !floating ? 'transparent' : undefined,
+      },
+    },
+  })
+);
+
+interface Props {
+  label: string;
+  placeholder: string;
+  onChange: (value: string) => void;
+  value: string;
+}
+
+export const TextInputWithFloatingLabel: FC<Props> = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { classes } = useStyles({
+    floating: props.value.trim().length !== 0 || focused,
+  });
+
+  return (
+    <TextInput
+      label={props.label}
+      placeholder={props.placeholder}
+      required
+      classNames={classes}
+      value={props.value}
+      onChange={(event) => props.onChange(event.currentTarget.value)}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      mt="md"
+      autoComplete="nope"
+    />
+  );
+};
