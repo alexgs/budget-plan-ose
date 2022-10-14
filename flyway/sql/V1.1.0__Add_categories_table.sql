9b7323797310dabbd853b78ea321802c3e251b1a
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-14 07:03:23 -0400

    Add Flyway migration for `categories` table

---
+++ flyway/sql/V1.1.0__Add_categories_table.sql
@@ -0,0 +1,19 @@
+--[ # TABLE public.categories # ]--
+
+CREATE TABLE IF NOT EXISTS public.categories
+(
+  id         UUID                           DEFAULT uuid_generate_v4() NOT NULL
+    CONSTRAINT categories_pk
+      PRIMARY KEY,
+  name       TEXT                                                      NOT NULL,
+  parent_id  UUID,
+  slug       TEXT,
+  created_at TIMESTAMP(3) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+  updated_at TIMESTAMP(3) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP  NOT NULL
+);
+
+CREATE TRIGGER set_categories_updated_at
+  BEFORE UPDATE
+  ON categories
+  FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
