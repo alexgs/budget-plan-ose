2517cf6292ea4c2edbe3a390ff34622a520dc8d9
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-14 15:17:41 -0400

    Add SWR

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,33 +1,14 @@
-import { MantineProvider, MantineThemeOverride } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import type { AppProps as DefaultAppProps } from 'next/app';
 import { NextComponentType, NextPageContext } from 'next/dist/shared/lib/utils';
 import { Session } from 'next-auth';
 import { SessionProvider, useSession } from 'next-auth/react';
 import React, { ReactNode } from 'react';
+import { SWRConfig } from 'swr';
 
 import { theme } from '../components/mantine-theme';
 
 import '@fontsource/inter/variable.css';
-
-interface AppProps extends DefaultAppProps<{ session: Session }> {
-  Component: NextComponentType<NextPageContext, any, any> & { isPublic?: boolean };
-}
-
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-        {Component.isPublic ? (
-          <Component {...pageProps} />
-        ) : (
-          <Auth>
-            <Component {...pageProps} />
-          </Auth>
-        )}
-      </MantineProvider>
-    </SessionProvider>
-  );
-}
 
 interface AuthProps {
   children: ReactNode | ReactNode[];
@@ -45,3 +26,35 @@ const Auth: React.FC<AuthProps> = (props: AuthProps) => {
 
   return <>{children}</>;
 };
+
+const globalFetcher = (resource: RequestInfo | URL, init?: RequestInit) =>
+  fetch(resource, init).then((res) => res.json());
+
+interface AppProps extends DefaultAppProps<{ session: Session }> {
+  Component: NextComponentType<NextPageContext, any, any> & {
+    isPublic?: boolean;
+  };
+}
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const content = Component.isPublic ? (
+    <Component {...pageProps} />
+  ) : (
+    <Auth>
+      <Component {...pageProps} />
+    </Auth>
+  );
+
+  return (
+    <SessionProvider session={session}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <SWRConfig value={{ fetcher: globalFetcher }}>
+          {content}
+        </SWRConfig>
+      </MantineProvider>
+    </SessionProvider>
+  );
+}
