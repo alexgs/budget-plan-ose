733d68dbe5edc794f499543d9ab8ca32dbf34d6a
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-17 07:01:55 -0400

    Update database schema

--- database/src/seed.ts
+++ database/src/seed.ts
@@ -1,21 +1,53 @@
-import * as dotenv from "dotenv";
-import * as dotenvExpand from "dotenv-expand";
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
 
-const envVars = dotenv.config({ path: "../.env" });
+const envVars = dotenv.config({ path: '../.env' });
 dotenvExpand.expand(envVars);
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-import categories from "./seed-data/categories.json" assert { type: 'json' };
+import categories from './seed-data/categories.json' assert { type: 'json' };
+import users from './seed-data/users.json' assert { type: 'json' };
+import userAccounts from './seed-data/user_accounts.json' assert { type: 'json' };
 
 const prisma = new PrismaClient();
 
-async function seed() {
+async function createCategories() {
   return Promise.all(
     categories.map((data) => {
       return prisma.category.create({ data });
     })
   );
+}
+
+async function upsertUsers() {
+  return Promise.all(
+    users.map((data) => {
+      return prisma.user.upsert({
+        where: { id: data.id },
+        create: data,
+        update: data,
+      });
+    })
+  );
+}
+
+async function upsertUserAccounts() {
+  return Promise.all(
+    userAccounts.map((data) => {
+      return prisma.account.upsert({
+        where: { id: data.id },
+        create: data,
+        update: data,
+      });
+    })
+  );
+}
+
+async function seed() {
+  await upsertUsers();
+  await upsertUserAccounts();
+  await createCategories();
 }
 
 seed()
