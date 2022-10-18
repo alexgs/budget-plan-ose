f24e6156c4b258a1a798c217853d97451370793c
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-18 06:46:56 -0400

    Add FontAwesome

--- webapp/components/AddTransactionButton.tsx
+++ webapp/components/AddTransactionButton.tsx
@@ -2,6 +2,8 @@
  * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusLarge } from '@fortawesome/pro-solid-svg-icons'
 import { Affix, Button } from '@mantine/core';
 import { FC } from 'react';
 
@@ -14,7 +16,7 @@ export const AddTransactionButton: FC = () => {
             console.log('>> click <<');
           }}
         >
-          +
+          <FontAwesomeIcon icon={faPlusLarge} />
         </Button>
       </Affix>
     </>
