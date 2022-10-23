16acdb99da9e7bfc3dbcad1946d8376b73fef316
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-23 17:15:00 -0400

    Add stub "new deposit" page

---
+++ webapp/pages/transactions/deposit.tsx
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
+ */
+
+import { FC } from 'react';
+import { Page } from '../../components';
+
+const Deposit: FC = () => {
+  return (
+    <Page>
+      <div>Hello deposit!</div>
+    </Page>
+  );
+};
+
+export default Deposit;
