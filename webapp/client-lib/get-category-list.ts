64e5f392b26e7c7fea5981f4f0ae8925a17b2066
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-10-23 17:23:17 -0400

    Get a list of all categories

--- webapp/client-lib/get-category-list.ts
+++ webapp/client-lib/get-category-list.ts
@@ -6,21 +6,30 @@ import { buildCategoryTree } from './build-category-tree';
 import { CategoryTreeNode, RawCategory } from './types';
 
 interface categoryValues {
-  id: string; // UUID of this category
   balance: number | null; // Current balance in cents
+  depth: number;
+  id: string; // UUID of this category
+  isLeaf: boolean;
   label: string; // Slash-separated joining of category's name with parent's name
 }
 
 function visitLeaves(
   output: categoryValues[],
   cats: CategoryTreeNode[],
-  parentLabel: string | null = null
+  parentLabel: string | null = null,
+  depth: number = 0
 ) {
   cats.forEach((cat) => {
     const label = parentLabel ? parentLabel + '/' + cat.name : cat.name;
     const { id, balance } = cat;
-    output.push({ id, balance, label });
-    visitLeaves(output, cat.children, label);
+    output.push({
+      balance,
+      depth,
+      id,
+      label,
+      isLeaf: cat.children.length === 0,
+    });
+    visitLeaves(output, cat.children, label, depth + 1);
   });
   return output;
 }
--- webapp/pages/transactions/deposit.tsx
+++ webapp/pages/transactions/deposit.tsx
@@ -2,10 +2,37 @@
  * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import { faTriangleExclamation } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Alert, Loader } from '@mantine/core';
 import { FC } from 'react';
+import useSWR from 'swr';
+import { getCategoryList } from '../../client-lib';
+
 import { Page } from '../../components';
 
 const Deposit: FC = () => {
+  // Get sorted categories and balances
+  const { error, data: catData } = useSWR('/api/categories');
+  if (error) {
+    console.error(error);
+    return (
+      <Alert
+        color="red"
+        icon={<FontAwesomeIcon icon={faTriangleExclamation} />}
+        title="Error!"
+      >
+        A network error occurred. Please check the console logs for details.
+      </Alert>
+    );
+  }
+
+  if (!catData) {
+    return <Loader variant="bars" />;
+  }
+
+  const categories = getCategoryList(catData);
+  console.log(categories);
   return (
     <Page>
       <div>Hello deposit!</div>
