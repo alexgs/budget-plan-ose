07db9e388028a74dd96661771c53075fa1727bbb
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-11-12 09:43:44 -0500

    Tighten security

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -5,14 +5,34 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import * as env from 'env-var';
 import NextAuth from 'next-auth';
+import { CallbacksOptions } from 'next-auth/core/types';
 import GithubProvider from 'next-auth/providers/github';
 
 import prisma from '../../../server-lib/prismadb';
+
+type SignInCallbackArgs = Parameters<CallbacksOptions['signIn']>[0];
+
+async function signInCallback(args: SignInCallbackArgs) {
+  const { account } = args;
+  // noinspection RedundantIfStatementJS
+  if (
+    account &&
+    account.provider === env.get('ALLOWED_PROVIDER').required().asString() &&
+    account.providerAccountId ===
+      env.get('ALLOWED_USER_ID').required().asString()
+  ) {
+    return true;
+  }
+  return false;
+}
 
 // TODO GitHub provider doesn't pull in my email address; maybe needs a scope or something? :shrug:
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    signIn: signInCallback,
+  },
   pages: {
     error: '/auth/error',
     signIn: '/auth/login',
