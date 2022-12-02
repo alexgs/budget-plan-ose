d98cf55479053a61c775eb705b43db69e7057d58
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-12-02 18:32:04 -0500

    Add Jest and working smoke test

---
+++ webapp/client-lib/get-category-list.test.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
+ */
+
+export {};
+
+describe('Smoke test', () => {
+  it('works', () => {
+    expect(1+1).toEqual(2);
+  });
+});
