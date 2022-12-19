d573f33ddfeab3f8a24c19f8001cc82070c66687
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-12-19 17:05:12 -0500

    BPL-5: Add account types to schema and constants

---
+++ webapp/shared-lib/constants.ts
@@ -0,0 +1,6 @@
+export const ACCOUNT_TYPES = {
+  CREDIT_CARD: 'account-types.credit-card',
+  CHECKING: 'account-types.checking',
+  SAVINGS: 'account-types.savings',
+  OTHER: 'account-types.other'
+}
