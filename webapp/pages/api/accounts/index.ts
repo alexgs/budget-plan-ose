4096826ddf496cf824e5c3c906df0d230739f5a9
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-12-28 15:24:56 -0500

    BPL-4: Add API handler for creating new accounts

--- webapp/pages/api/accounts/index.ts
+++ webapp/pages/api/accounts/index.ts
@@ -4,9 +4,18 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth/next';
+import { InferType, ValidationError } from 'yup';
+import * as yup from 'yup';
 
 import { authOptions } from '../auth/[...nextauth]';
 import { prisma } from '../../../server-lib';
+
+const newAccountSchema = yup.object({
+  accountType: yup.string().required(), // TODO This can be a more specific filter
+  description: yup.string().required(),
+});
+
+type NewAccountSchema = InferType<typeof newAccountSchema>;
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,8 +27,32 @@ export default async function handler(
     if (req.method === 'GET') {
       const accounts = await prisma.financialAccount.findMany();
       res.send(accounts);
+    } else if (req.method === 'POST') {
+      let payload: NewAccountSchema = { accountType: '', description: '' };
+      try {
+        payload = await newAccountSchema.validate(req.body);
+      } catch (e: any) {
+        if (e.name && e.name === 'ValidationError') {
+          const error: ValidationError = e as ValidationError;
+          console.error(`>> POST /api/accounts 400 ${error.errors} <<`);
+          res
+            .status(400)
+            .send(
+              'Error: payload failed validation. Please check server logs.'
+            );
+        } else {
+          console.error(`>> Unknown error: ${e} <<`);
+          res.status(500).send('Unknown error. Please check server logs.');
+        }
+        return;
+      }
+
+      const newAccount = await prisma.financialAccount.create({
+        data: payload,
+      });
+      res.send(newAccount);
     } else {
-      res.status(405).setHeader('Allow', 'GET').send('Method not allowed.');
+      res.status(405).setHeader('Allow', 'GET POST').send('Method not allowed.');
     }
   } else {
     res.status(400).send('Bad request.');
