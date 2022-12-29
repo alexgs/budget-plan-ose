e792d01c87367621127b28a4cd84dd96c4eb2d2b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-12-29 09:43:50 -0500

    BPL-4: Implement patch handler for accounts

--- webapp/pages/api/accounts/[accountId].ts
+++ webapp/pages/api/accounts/[accountId].ts
@@ -1,14 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth/next';
-import { InferType } from 'yup';
+import { InferType, ValidationError } from 'yup';
 import * as yup from 'yup';
 
-import { nextAuthOptions } from '../../../server-lib';
+import { nextAuthOptions, prisma } from '../../../server-lib';
 
 const patchAccountSchema = yup.object({
-  accountType: yup.string().nullable(), // TODO This can be a more specific filter
-  description: yup.string().nullable(),
-  id: yup.string().required(),
+  accountType: yup.string(), // TODO This can be a more specific filter
+  description: yup.string(),
 });
 
 type PatchAccountSchema = InferType<typeof patchAccountSchema>;
@@ -21,7 +20,38 @@ export default async function handler(
 
   if (session) {
     if (req.method === 'PATCH') {
-      // TODO
+      let payload: PatchAccountSchema = {
+        accountType: '',
+        description: '',
+      };
+      let accountId = '';
+      try {
+        accountId = await yup.string().required().validate(req.query.accountId);
+        payload = await patchAccountSchema.validate(req.body);
+      } catch (e: any) {
+        if (e.name && e.name === 'ValidationError') {
+          const error: ValidationError = e as ValidationError;
+          console.error(`>> POST /api/accounts/:id 400 ${error.errors} <<`);
+          res
+            .status(400)
+            .send(
+              'Error: payload failed validation. Please check server logs.'
+            );
+        } else {
+          console.error(`>> Unknown error: ${e} <<`);
+          res.status(500).send('Unknown error. Please check server logs.');
+        }
+        return;
+      }
+
+      const patchedAccount = await prisma.financialAccount.update({
+        where: { id: accountId },
+        data: {
+          accountType: payload.accountType,
+          description: payload.description,
+        },
+      });
+      res.send(patchedAccount);
     } else {
       res.status(405).setHeader('Allow', 'PATCH').send('Method not allowed.');
     }
