35b409ad301b821c3987f8914913558d87776b9f
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-12-30 16:34:43 -0500

    BPL-27: Create credit card payment category automatically

--- webapp/shared-lib/types.ts
+++ webapp/shared-lib/types.ts
@@ -9,7 +9,11 @@ import { schemaObjects } from './schema-objects';
 
 export namespace SchemaTypes {
   export type NewAccount = InferType<typeof schemaObjects.newAccount>;
-  export type NewCategory = InferType<typeof schemaObjects.newCategory>;
+  export type NewCategory = InferType<typeof schemaObjects.newCategory> & {
+    // TODO This might be a code smell. Maybe refactor to have a type based on
+    //   the Yup schema and another type based on the Prisma input object.
+    isSystem?: boolean
+  };
   export type NewTransaction = InferType<typeof schemaObjects.newTransaction>;
 }
 
