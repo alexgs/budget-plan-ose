20fc25b009db3edc887249e0e9bda45e4206354f
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-12-30 09:23:58 -0500

    BPL-20: Update schema to use `is_system` columns

--- flyway/sql/V1.1.0__Add_account_type.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
- */
-
---[ ## TABLE public.financial_accounts ## ]--
-
-ALTER TABLE public.financial_accounts
-  ADD account_type TEXT NOT NULL default 'account-types.other'
-;
