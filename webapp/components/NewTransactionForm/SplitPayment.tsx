fb44d88847a9f33db491828d147b40b90eadaf65
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-01-21 09:04:48 -0500

    BPL-35: Show just one account field on the split transaction form

--- webapp/components/NewTransactionForm/SplitPayment.tsx
+++ webapp/components/NewTransactionForm/SplitPayment.tsx
@@ -2,10 +2,12 @@
  * Copyright 2022-23 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import styled from '@emotion/styled';
 import { Checkbox, Group } from '@mantine/core';
 import React from 'react';
 
 import { NewTransactionFormHook } from '../../client-lib/types';
+import { font } from '../tokens';
 
 import { AmountContainer, SplitAmount } from './Amounts';
 import { SaveButton, SplitButton } from './Buttons';
@@ -21,6 +23,10 @@ import {
 //   '-' prefix (or maybe some other FontAwesome icon) so there's another visual
 //   indicator, too.
 
+const NoticeText = styled.div({
+  fontSize: font.size.small,
+});
+
 interface Props {
   accounts: { label: string; value: string }[];
   categories: { label: string; value: string }[];
@@ -32,11 +38,6 @@ export const SplitPayment: React.FC<Props> = (props) => {
   function renderAmounts() {
     return props.mantineForm.values.amounts.map((amount, index) => (
       <AmountContainer key={`amount.${index}`}>
-        <AccountField
-          accounts={props.accounts}
-          index={index}
-          mantineForm={props.mantineForm}
-        />
         <CategoryField
           categories={props.categories}
           index={index}
@@ -51,6 +52,11 @@ export const SplitPayment: React.FC<Props> = (props) => {
   return (
     <>
       <DescriptionField mantineForm={props.mantineForm} />
+      <AccountField accounts={props.accounts} mantineForm={props.mantineForm} />
+      <NoticeText>
+        Multiple accounts for split transactions are disabled until we have a
+        solid use-case to implement.
+      </NoticeText>
       <SplitAmount mantineForm={props.mantineForm} />
       <Checkbox
         label="Credit or deposit"
