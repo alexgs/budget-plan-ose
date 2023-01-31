5ab12c7b923d49ddb6303332403dd6c30add3b93
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-01-31 16:47:50 -0500

    BPL-2: Add `countChildCategories` function

--- webapp/server-lib/database/index.ts
+++ webapp/server-lib/database/index.ts
@@ -2,6 +2,7 @@
  * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import { countChildCategories } from './count-child-categories';
 import { createAccount } from './create-account';
 import { createCategory } from './create-category';
 import { ensureAccount } from './ensure-account';
@@ -13,6 +14,7 @@ import { getTransactions } from './get-transactions';
 import { saveTransaction } from './save-transaction';
 
 export const database = {
+  countChildCategories,
   createAccount,
   createCategory,
   ensureAccount,
