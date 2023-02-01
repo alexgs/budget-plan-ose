1a234d943b3a5ea8085c3e0b071fc00fe8b14366
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-01 13:15:44 -0500

    BPL-15: Get the modal to display when the edit button is clicked

--- webapp/components/AddEditCategoryModal/EditCategoryButton.tsx
+++ webapp/components/AddEditCategoryModal/EditCategoryButton.tsx
@@ -4,15 +4,43 @@
 
 import { faPencil } from '@fortawesome/pro-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { UnstyledButton } from '@mantine/core';
+import { Modal, UnstyledButton } from '@mantine/core';
 import React from 'react';
+import { CategoryModal } from './CategoryModal';
 
 interface Props {}
 
 export const EditCategoryButton: React.FC<Props> = (props) => {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  function handleButtonClick(): void {
+    setIsVisible(true);
+  }
+
+  function handleModalClose(): void {
+    setIsVisible(false);
+  }
+
+  function renderModalContent() {
+    if (isVisible) {
+      return <CategoryModal onClose={handleModalClose} />;
+    }
+    return null;
+  }
+
   return (
-    <UnstyledButton>
-      <FontAwesomeIcon icon={faPencil} />
-    </UnstyledButton>
+    <div>
+      <UnstyledButton onClick={handleButtonClick}>
+        <FontAwesomeIcon icon={faPencil} />
+      </UnstyledButton>
+      <Modal
+        onClose={handleModalClose}
+        opened={isVisible}
+        overlayBlur={3}
+        title="Edit category"
+      >
+        {renderModalContent()}
+      </Modal>
+    </div>
   );
 };
