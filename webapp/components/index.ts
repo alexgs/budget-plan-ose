a9a400a4c3ed81a1451c43b742acb02a7aafe106
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-01 13:00:02 -0500

    BPL-15: Rename directory

--- webapp/components/index.ts
+++ webapp/components/index.ts
@@ -3,7 +3,7 @@
  */
 
 export { EditAccountButton, NewAccountButton } from './AddEditAccountModal';
-export { AddCategory } from './AddCategory';
+export { AddCategory } from './AddEditCategoryModal';
 export { AddTransactionButton } from './AddTransactionButton';
 export { DepositForm } from './DepositForm';
 export { NewTransactionForm } from './NewTransactionForm';
