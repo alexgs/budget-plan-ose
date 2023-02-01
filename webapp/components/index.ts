571d7f28323e2c99af9c9247f1d8091b77ada7bf
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-01 12:59:33 -0500

    BPL-15: Rename directory

--- webapp/components/index.ts
+++ webapp/components/index.ts
@@ -2,7 +2,7 @@
  * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
-export { EditAccountButton, NewAccountButton } from './AccountModal';
+export { EditAccountButton, NewAccountButton } from './AddEditAccountModal';
 export { AddCategory } from './AddCategory';
 export { AddTransactionButton } from './AddTransactionButton';
 export { DepositForm } from './DepositForm';
