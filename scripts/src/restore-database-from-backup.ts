e06f402b33707cdc5cdfaf220e8902bbd93a97cc
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-12 19:01:35 -0500

    BPL-1: Add confirmation prompt

--- scripts/src/restore-database-from-backup.ts
+++ scripts/src/restore-database-from-backup.ts
@@ -2,10 +2,11 @@
  * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import chalk from 'chalk';
 import fs from 'fs/promises';
 import inquirer from 'inquirer'; // We use an old version of `inquirer` to make it work with TypeScript and `ts-node`
 
-import { BACKUP_DIR } from './constants';
+import { BACKUP_DIR, DATABASE } from './constants';
 
 async function main() {
   const unfilteredFiles = await fs.readdir(BACKUP_DIR);
@@ -21,8 +22,24 @@ async function main() {
       choices: files,
     },
   ]);
-  console.log('\nYour answer:');
-  console.log(answer1.filename);
+
+  console.log(
+    '\nYou are about to overwrite the database',
+    chalk.yellow.bold(DATABASE.NAME),
+    'on',
+    chalk.yellow.bold(DATABASE.HOST),
+    'with the data in',
+    `${chalk.cyan.bold(answer1.filename)}.`
+  );
+  const answer2 = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirm',
+      message: 'Are you sure?',
+      default: false,
+    }
+  ]);
+  console.log(`Your answer: ${answer2.confirm}`);
 }
 
 main()
