b5cb30ae82aaf1501744e0cc6eecfe132b55dc32
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-20 19:02:18 -0500

    BPL-47: Add new Flyway schema

--- flyway/sql/V1.2.0__Track_account_balance.sql
+++ flyway/sql/V1.2.0__Track_account_balance.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
+ * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
 --[ ## BPL-33 ## ]--
