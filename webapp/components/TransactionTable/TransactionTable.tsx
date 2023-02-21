eb18ec3f1852bc65f0ea108069b1b78a03b4fa7c
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-21 08:31:27 -0500

    BPL-46: Remove @tanstack/react-table

--- webapp/components/TransactionTable/TransactionTable.tsx
+++ webapp/components/TransactionTable/TransactionTable.tsx
@@ -3,85 +3,27 @@
  */
 
 import { Table } from '@mantine/core';
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from '@tanstack/react-table';
 import React from 'react';
 
-import { formatAmount, formatClientDate } from '../../client-lib';
 import { TransactionRow } from '../../client-lib/types';
 
-
-const columnHelper = createColumnHelper<TransactionRow>();
-const columns = [
-  columnHelper.accessor('date', {
-    cell: (info) => formatClientDate(info.getValue()),
-    header: () => <span>Date</span>,
-  }),
-  columnHelper.accessor('account', {
-    cell: (info) => info.getValue(),
-    header: () => <span>Account</span>,
-  }),
-  columnHelper.accessor('description', {
-    cell: (info) => info.getValue(),
-    header: () => <span>Description</span>,
-  }),
-  columnHelper.accessor('category', {
-    cell: (info) => info.getValue(),
-    header: () => <span>Category</span>,
-  }),
-  columnHelper.accessor('amount', {
-    cell: (info) => formatAmount(info.getValue()),
-    header: () => <span>Amount</span>,
-  }),
-  // columnHelper.accessor('status', {
-  //   cell: (info) => info.getValue(),
-  //   header: () => <span>Status</span>,
-  // }),
-];
-
 interface Props {
-  data: TransactionRow[]
+  data: TransactionRow[];
 }
 
 export const TransactionTable: React.FC<Props> = (props) => {
-  const table = useReactTable({
-    columns,
-    data: props.data,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
-  function renderHeaders() {
-    return table.getFlatHeaders().map((header) => {
-      return (
-        <th key={header.id}>
-          {flexRender(header.column.columnDef.header, header.getContext())}
-        </th>
-      );
-    });
-  }
-
-  function renderRows() {
-    return table.getRowModel().rows.map((row) => (
-      <tr key={row.id}>
-        {row.getVisibleCells().map((cell) => (
-          <td key={cell.id}>
-            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-          </td>
-        ))}
-      </tr>
-    ));
-  }
-
   return (
     <Table>
       <thead>
-        <tr>{renderHeaders()}</tr>
+        <tr>
+          <td>Nothing to see here</td>
+        </tr>
       </thead>
-      <tbody>{renderRows()}</tbody>
+      <tbody>
+        <tr>
+          <td>Nothing to see here</td>
+        </tr>
+      </tbody>
     </Table>
   );
 };
