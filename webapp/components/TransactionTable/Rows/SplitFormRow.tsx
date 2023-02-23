943f907995ac4b1697f9e6cbf3c8beec92700990
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-23 10:09:57 -0500

    BPL-39: Wire `SplitFormRow` to track total balance and balance remaining

--- webapp/components/TransactionTable/Rows/SplitFormRow.tsx
+++ webapp/components/TransactionTable/Rows/SplitFormRow.tsx
@@ -2,20 +2,62 @@
  * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import styled from '@emotion/styled';
 import { faFloppyDisk, faSplit } from '@fortawesome/pro-regular-svg-icons';
+import { faDollarSign } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Loader, TextInput, UnstyledButton } from '@mantine/core';
+import {
+  Button,
+  Checkbox,
+  Loader,
+  NumberInput,
+  TextInput,
+  UnstyledButton,
+} from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
 import React from 'react';
 
+import { formatAmount } from '../../../client-lib';
 import { NewTransactionFormHook } from '../../../client-lib/types';
-import { Account, Category } from '../../../shared-lib';
+import { Account, Category, dollarsToCents } from '../../../shared-lib';
+import { amountStyle } from '../../NewTransactionForm/Amounts/amount-style';
 import {
   AccountField,
   AmountField,
   CategoryField,
   CreditField,
 } from '../BuildingBlocks';
+
+// TODO Use a `NumberInput` here and format it like the "Total Amount" field
+const AmountRemainingAmount = styled.div({
+  backgroundColor: 'rgb(37, 38, 43)',
+  border: 'solid rgb(55, 58, 64) 1px',
+  borderRadius: 3,
+  fontSize: 14,
+  padding: '6px 12px',
+});
+
+const AmountRemainingLabel = styled.div({
+  fontSize: 14,
+});
+
+interface Subrecord {
+  amount: number;
+  isCredit: boolean;
+}
+
+/**
+ * Takes a bunch of values from the form and returns their sum. The output is
+ * negative for debits and positive for credits.
+ */
+const sumSubrecords = (subrecords: Subrecord[]): number => {
+  return subrecords.reduce((output, current) => {
+    if (current.isCredit) {
+      return output + current.amount;
+    }
+    return output - current.amount;
+  }, 0);
+};
 
 interface Props {
   accountData: Account[];
@@ -28,6 +70,19 @@ interface Props {
 }
 
 export const SplitFormRow: React.FC<Props> = (props) => {
+  function calcAmountRemaining() {
+    if (props.mantineForm.values.isCredit) {
+      return (
+        props.mantineForm.values.balance -
+        sumSubrecords(props.mantineForm.values.categories)
+      );
+    }
+    return (
+      props.mantineForm.values.balance +
+      sumSubrecords(props.mantineForm.values.categories)
+    );
+  }
+
   if (props.isSaving) {
     return (
       <tr>
@@ -66,25 +121,29 @@ export const SplitFormRow: React.FC<Props> = (props) => {
             {...props.mantineForm.getInputProps('description')}
           />
         </td>
-        <td style={{ textAlign: 'center' }}>
-          <CategoryField
-            categoryData={props.categoryData}
-            mantineForm={props.mantineForm}
-          />
-          <Button compact onClick={props.onSplitCategory} variant="subtle">
-            <FontAwesomeIcon icon={faSplit} />
-            &nbsp; Split
-          </Button>
+        <td>
+          <AmountRemainingLabel>Amount Remaining:</AmountRemainingLabel>
+          <AmountRemainingAmount>
+            {formatAmount(dollarsToCents(calcAmountRemaining()))}
+          </AmountRemainingAmount>
         </td>
         <td>{/* Notes */}</td>
         <td>
-          <AmountField
-            mantineForm={props.mantineForm}
-            subrecordType={'category'}
+          <NumberInput
+            decimalSeparator="."
+            hideControls
+            icon={<FontAwesomeIcon icon={faDollarSign} />}
+            my="sm"
+            precision={2}
+            required
+            sx={props.mantineForm.values.isCredit ? amountStyle : {}}
+            {...props.mantineForm.getInputProps('balance')}
           />
-          <CreditField
-            mantineForm={props.mantineForm}
-            subrecordType={'category'}
+          <Checkbox
+            label="Credit or deposit"
+            {...props.mantineForm.getInputProps('isCredit', {
+              type: 'checkbox',
+            })}
           />
         </td>
         <td>
