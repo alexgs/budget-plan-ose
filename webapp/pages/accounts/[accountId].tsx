62f75136086cbb786f5c2fe48e3ad0527bad0742
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-02-23 19:33:43 -0500

    BPL-43: Update account page to be clone of transaction table

--- webapp/pages/accounts/[accountId].tsx
+++ webapp/pages/accounts/[accountId].tsx
@@ -2,13 +2,55 @@
  * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import { faTriangleExclamation } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Alert, Loader } from '@mantine/core';
 import React from 'react';
+import useSWR from 'swr';
 import { Page, TransactionTable } from '../../components';
+import { Account, ApiSchema, Category } from '../../shared-lib';
 
 const AccountDetail: React.FC = () => {
+  const { error: txnError, data: txnData } = useSWR<ApiSchema.Transaction[]>(
+    '/api/transactions',
+    { refreshInterval: 1000 }
+  );
+
+  const { error: accountError, data: accountData } = useSWR<Account[]>(
+    '/api/accounts',
+    { refreshInterval: 1000 }
+  );
+
+  const { error: categoryError, data: categoryData } = useSWR<Category[]>(
+    '/api/categories',
+    { refreshInterval: 1000 }
+  );
+
+  const anyError = accountError ?? categoryError ?? txnError;
+  if (anyError) {
+    console.error(anyError);
+    return (
+      <Alert
+        color="red"
+        icon={<FontAwesomeIcon icon={faTriangleExclamation} />}
+        title="Error!"
+      >
+        A network error occurred. Please check the console logs for details.
+      </Alert>
+    );
+  }
+
+  if (!accountData || !categoryData || !txnData) {
+    return <Loader variant="bars" />;
+  }
+
   return (
     <Page>
-      <TransactionTable />
+      <TransactionTable
+        accountData={accountData}
+        categoryData={categoryData}
+        txnData={txnData}
+      />
     </Page>
   );
 };
