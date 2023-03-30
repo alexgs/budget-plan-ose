20c6acb4c2ce46f1c71c78b34ff819297e8cd26b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-03-30 06:56:08 -0400

    BPL-49: Render basic rows

--- webapp/components/TransactionTable/TransactionTableDisplay.tsx
+++ webapp/components/TransactionTable/TransactionTableDisplay.tsx
@@ -2,8 +2,16 @@
  * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import styled from '@emotion/styled';
 import React from 'react';
+
 import { Account, ApiSchema, Category } from '../../shared-lib';
+
+const Column = styled.div({});
+
+const Row = styled.div({});
+
+const Table = styled.div({});
 
 interface Props {
   accountData: Account[];
@@ -13,5 +21,15 @@ interface Props {
 }
 
 export const TransactionTableDisplay: React.FC<Props> = (props) => {
-  return <div>Hello display component</div>
+  function renderRows() {
+    return props.txnData.map((txn) => {
+      return (
+        <Row key={txn.id}>
+          <Column>{txn.id}</Column>
+        </Row>
+      );
+    });
+  }
+
+  return <Table>{renderRows()}</Table>;
 };
