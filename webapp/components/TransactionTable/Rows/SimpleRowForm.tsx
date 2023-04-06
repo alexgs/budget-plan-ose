faa1650738e944adae980f80fa8aa43d62c79b9e
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-04-06 16:36:05 -0500

    BPL-49: Start wiring up the new form fields

--- webapp/components/TransactionTable/Rows/SimpleRowForm.tsx
+++ webapp/components/TransactionTable/Rows/SimpleRowForm.tsx
@@ -2,12 +2,19 @@
  * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
+import { NativeSelect } from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
+import { useForm, UseFormReturnType, yupResolver } from '@mantine/form';
 import React from 'react';
 
 import {
+  AMOUNT_STATUS,
+  TRANSACTION_TYPES,
   ApiSchema,
+  TransactionType,
   getFriendlyAccountName,
-  getFriendlyCategoryName
+  getFriendlyCategoryName,
+  schemaObjects,
 } from '../../../shared-lib';
 import {
   AccountCell,
@@ -28,27 +35,64 @@ interface Props extends Omit<RowProps, 'txn'> {
 }
 
 export const SimpleRowForm: React.FC<Props> = (props) => {
+  const form: UseFormReturnType<ApiSchema.NewTransaction> = useForm({
+    initialValues: {
+      accounts: [
+        {
+          accountId:
+            props.data?.accounts[0].accountId ?? props.accountData[0].id,
+          amount: props.data?.accounts[0].amount ?? 0,
+          isCredit: props.data?.accounts[0].isCredit ?? (false as boolean),
+          status: props.data?.accounts[0].status ?? AMOUNT_STATUS.PENDING,
+        },
+      ],
+      categories: [
+        {
+          amount: props.data?.categories[0].amount ?? 0,
+          categoryId:
+            props.data?.categories[0].categoryId ?? props.categoryData[0].id,
+          isCredit: props.data?.categories[0].isCredit ?? (false as boolean),
+        },
+      ],
+      date: props.data?.date ?? new Date(),
+      description: props.data?.description ?? '',
+      type: props.data?.type ?? (TRANSACTION_TYPES.PAYMENT as TransactionType),
+    },
+    validate: yupResolver(schemaObjects.newTransaction),
+    validateInputOnChange: true,
+  });
+
+  function handleSubmit() {}
+
+  const accounts = props.accountData.map((account) => ({
+    value: account.id,
+    label: account.description,
+  }));
   const key = props.data ? props.data.id : 'new-txn';
   return (
-    <Row key={key}>
-      <ChevronCell>{/* Checkbox */}</ChevronCell>
-      <DateCell>{props.data?.date.toISOString()}</DateCell>
-      <AccountCell>
-        {getFriendlyAccountName(
-          props.accountData,
-          props.data.accounts[0].accountId
-        )}
-      </AccountCell>
-      <DescriptionCell>{props.data.description}</DescriptionCell>
-      <CategoryCell>
-        {getFriendlyCategoryName(
-          props.categoryData,
-          props.data.categories[0].categoryId
-        )}
-      </CategoryCell>
-      <NotesCell>{/* Notes */}</NotesCell>
-      <SmartAmountCell subrecord={props.data.categories[0]} />
-      <ButtonsCell>{/* Buttons */}</ButtonsCell>
-    </Row>
+    <form
+      onSubmit={form.onSubmit(handleSubmit, (values) => console.error(values))}
+    >
+      <Row key={key}>
+        <ChevronCell>{/* Checkbox */}</ChevronCell>
+        <DateCell>
+          <DatePicker
+            allowFreeInput
+            inputFormat="YYYY-MM-DD"
+            my="sm"
+            required
+            {...form.getInputProps('date')}
+          />
+        </DateCell>
+        <AccountCell>
+          <NativeSelect
+            data={accounts}
+            my="sm"
+            required
+            {...form.getInputProps('accounts.0.accountId')}
+          />
+        </AccountCell>
+      </Row>
+    </form>
   );
 };
