803d56a92b766a48c643b11d5a7e705c8f8db2f8
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-04-09 12:42:02 -0400

    BPL-49: Eliminate some duplicate code

--- webapp/components/TransactionTable/Components/MainAmountCell.tsx
+++ webapp/components/TransactionTable/Components/MainAmountCell.tsx
@@ -2,19 +2,10 @@
  * Copyright 2022-2023 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
  */
 
-import { faDollarSign } from '@fortawesome/pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Checkbox, CSSObject, MantineTheme, NumberInput } from '@mantine/core';
 import React from 'react';
 
 import { NewTransactionFormHook } from '../../../client-lib/types';
-
-import { AmountCell } from './Cell';
-
-const amountStyle = (theme: MantineTheme): CSSObject => ({
-  '.mantine-NumberInput-icon': { color: theme.colors.green[6] },
-  input: { color: theme.colors.green[4] },
-});
+import { AmountInputCell } from './AmountInputCell';
 
 interface Props {
   mantineForm: NewTransactionFormHook;
@@ -23,43 +14,24 @@ interface Props {
 export const MainAmountCell: React.FC<Props> = (props) => {
   if (props.mantineForm.values.categories.length > 1) {
     return (
-      <AmountCell style={{ display: 'flex', alignItems: 'center' }}>
-        <NumberInput
-          decimalSeparator="."
-          hideControls
-          icon={<FontAwesomeIcon icon={faDollarSign} />}
-          precision={2}
-          required
-          sx={props.mantineForm.values.isCredit ? amountStyle : {}}
-          {...props.mantineForm.getInputProps(`balance`)}
-        />
-        <Checkbox
-          label="Credit"
-          {...props.mantineForm.getInputProps('isCredit', {
-            type: 'checkbox',
-          })}
-        />
-      </AmountCell>
+      <AmountInputCell
+        checkboxInputProps={props.mantineForm.getInputProps(`isCredit`, {
+          type: 'checkbox',
+        })}
+        isCredit={props.mantineForm.values.isCredit}
+        numberInputProps={props.mantineForm.getInputProps(`balance`)}
+      />
     );
   }
 
   return (
-    <AmountCell style={{ display: 'flex', alignItems: 'center' }}>
-      <NumberInput
-        decimalSeparator="."
-        hideControls
-        icon={<FontAwesomeIcon icon={faDollarSign} />}
-        precision={2}
-        required
-        sx={props.mantineForm.values.categories[0].isCredit ? amountStyle : {}}
-        {...props.mantineForm.getInputProps(`categories.0.amount`)}
-      />
-      <Checkbox
-        label="Credit"
-        {...props.mantineForm.getInputProps('categories.0.isCredit', {
-          type: 'checkbox',
-        })}
-      />
-    </AmountCell>
+    <AmountInputCell
+      checkboxInputProps={props.mantineForm.getInputProps(
+        'categories.0.isCredit',
+        { type: 'checkbox' }
+      )}
+      isCredit={props.mantineForm.values.categories[0].isCredit}
+      numberInputProps={props.mantineForm.getInputProps(`categories.0.amount`)}
+    />
   );
 };
