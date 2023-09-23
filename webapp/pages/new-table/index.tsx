b90152cdaa42d87966bc8399674323c8a8f06af7
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-09-23 11:21:53 -0400

    BPL-68: Install tanstack/table; make a new page to hold the new table

---
+++ webapp/pages/new-table/index.tsx
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2022 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
+ */
+
+import { faTriangleExclamation } from '@fortawesome/pro-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Alert, Loader, Table } from '@mantine/core';
+import React from 'react';
+import useSWR from 'swr';
+
+import {
+  buildCategoryTree,
+  formatAmount,
+  getCategoryList,
+} from '../../client-lib';
+import {
+  AddCategoryButton,
+  AddSubcategoryButton,
+  EditCategoryButton,
+  Page,
+} from '../../components';
+import { space } from '../../components/tokens';
+import { Category } from '../../shared-lib';
+
+function NewTablePage() {
+  // Get sorted categories and balances
+  // const { error, data: catData } = useSWR<Category[]>('/api/categories', {
+  //   refreshInterval: 1000,
+  // });
+  // if (error) {
+  //   console.error(error);
+  //   return (
+  //     <Alert
+  //       color="red"
+  //       icon={<FontAwesomeIcon icon={faTriangleExclamation} />}
+  //       title="Error!"
+  //     >
+  //       A network error occurred. Please check the console logs for details.
+  //     </Alert>
+  //   );
+  // }
+  //
+  // if (!catData) {
+  //   return <Loader variant="bars" />;
+  // }
+
+  // const topLevelBalances = getCategoryList(buildCategoryTree(catData));
+  // const rows = topLevelBalances.map((row) => {
+  //   const data: Category | undefined = catData.find(
+  //     (value) => value.id === row.id
+  //   );
+  //   if (!data) {
+  //     // This is just for type safety, should never throw (in production)
+  //     throw new Error(`Unable to find data for category ID ${row.id}`);
+  //   }
+  //   const indentation = 10 * (row.depth + 1)
+  //   return (
+  //     <tr key={row.id}>
+  //       <td style={{paddingLeft: indentation}}>{row.label}</td>
+  //       <td style={{paddingLeft: indentation}}>{formatAmount(row.balance)}</td>
+  //       <td style={{ display: 'flex', justifyContent: 'space-around' }}>
+  //         <AddSubcategoryButton parentId={row.id} />
+  //         <EditCategoryButton data={data} />
+  //       </td>
+  //     </tr>
+  //   );
+  // });
+
+  return (
+    <Page>
+      <Table>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Balance</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Test</td>
+          </tr>
+        </tbody>
+      </Table>
+      <div style={{ marginTop: space.xl }}>
+        <AddCategoryButton />
+      </div>
+    </Page>
+  );
+}
+
+export default NewTablePage;
