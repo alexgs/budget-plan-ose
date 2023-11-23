74872292808915a8b29466143488db94372a8143
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-11-23 13:05:32 -0500

    BPL-120: Remove deprecated NextAuth method

--- webapp/pages/api/transactions/[txnRecordId].ts
+++ webapp/pages/api/transactions/[txnRecordId].ts
@@ -3,7 +3,7 @@
  */
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { unstable_getServerSession } from 'next-auth/next';
+import { getServerSession } from 'next-auth/next';
 import { ValidationError } from 'yup';
 
 import {
@@ -23,7 +23,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await unstable_getServerSession(req, res, nextAuthOptions);
+  const session = await getServerSession(req, res, nextAuthOptions);
 
   if (session) {
     if (req.method === 'POST') {
